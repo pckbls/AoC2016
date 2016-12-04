f922f1aa8dea99631031d139f3267d7175777a13
Patrick Bartels <pckbls@gmail.com>
2016-12-04 23:34:46 +0100

    Solve 2nd part of day 4

--- day4/main.py
+++ day4/main.py
@@ -8,6 +8,7 @@ import re
 
 def one(rooms):
     sector_id_sum = 0
+    valid_rooms = []
 
     for room in rooms:
         m = re.match('([a-z0-9\-]*)\[([a-z]*)\]', room)
@@ -27,20 +28,31 @@ def one(rooms):
         calculed_checksum=''.join([x[0] for x in sorted_letter_count][0:5])
 
         if checksum == calculed_checksum:
+            valid_rooms.append(('-'.join(tmp[0:-1]), sector_id))
             sector_id_sum += sector_id
 
     print(sector_id_sum)
+    return valid_rooms
 
 def two(rooms):
-    pass
+    for room, sector_id in rooms:
+        decoded_room = ''
+        for c in room:
+            if c == '-':
+                decoded_room += '-'
+            else:
+                decoded_room += chr((ord(c) - ord('a') + sector_id) % 26 + ord('a'))
+
+        if decoded_room == 'northpole-object-storage':
+            print(sector_id)
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         rooms = f.read().splitlines()
 
     print('Part one')
-    one(rooms)
+    valid_rooms = one(rooms)
 
     print('Part two')
-    two(rooms)
+    two(valid_rooms)
 
